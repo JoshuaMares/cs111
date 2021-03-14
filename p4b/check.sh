96c66c86b2e7de8232c747d790b9031bcde187bc
Josh <>
2021-03-14 03:41:42 -0700

    testing check script

--- p4b/check.sh
+++ p4b/check.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-echo "Temporary test script"
+./lab4b --period=5 --scale=F --log=templogfile <<-EOF
+SCALE=C
+PERIOD=3
+START
+STOP
+LOG THIS LINE
+SCALE=F
+OFF
+EOF
+
+if [ $? -eq 0 ]
+then
+  echo "No Errors"
+else
+  echo "Errors"
+fi
+
+for string in SCALE=C PERIOD=3 START STOP SCALE=F OFF SHUTDOWN
+  do
+    grep "$string" templogfile
+    if [ $? -eq 0 ]
+    then
+      echo "$string logged"
+    else
+      echo "$string not logged"
+    fi
+  done
+
+grep "LOG THIS LINE" templogfile
+if [ $? -eq 0 ]
+then
+  echo "Log command logged"
+else
+  echo "Log command not logged"
+fi
+
+rm -f templogfile
